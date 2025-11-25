7a3eb658b539f3130ceac4bbb0de7066bedc3329
kunduru <poojithakunduru7@gmail.com>
2025-11-25 22:35:14 +0530

    updated example  test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,21 +1,24 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('login test demoblaze', async ({ page }) => {
   await page.goto('https://demoblaze.com/');
 
-  // Expect a title "to contain" a substring.
+  // Validate title
   await expect(page).toHaveTitle('STORE');
-  
-  page.locator('#login2').click();
-  page.locator('#loginusername').fill('Reddyyyyyy');
-  page.locator('//*[@id="loginpassword"]').fill('kunduru');
-  //page.click('#logInModal > div > div > div.modal-footer > button.btn.btn-primary')
-  page.locator('button:has-text("Log in")');
-  //page.locator('button').filter({ hasText: 'Log in' })
+
+  // Click Login button
+  await page.locator('#login2').click();
+
+  // Fill username & password
+  await page.locator("//input[@id='loginusername']").fill('Reddyyyyyy');
+  await page.locator("//*[@id='loginpassword']").fill('kunduru');
+
+  // Click the "Log in" button
+  await page.locator('button:has-text("Log in")').click();
+
+  // Optional: Wait to see result
+  await page.waitForTimeout(3000);
+
   await page.close();
-
-
 });
-
-
